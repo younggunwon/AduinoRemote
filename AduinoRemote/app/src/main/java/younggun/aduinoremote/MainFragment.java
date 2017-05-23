bcf0765cd36c32ee5a2823ab347c6a4ad640a567
younggunwon <dnjsdudrjs@naver.com>
2017-05-23 20:27:57 +0900

    new
    new

---
+++ AduinoRemote/app/src/main/java/younggun/aduinoremote/MainFragment.java
@@ -0,0 +1,30 @@
+package younggun.aduinoremote;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by LOVE on 2017-05-23.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //주변 기기 검색
+            }
+        });
+        return v;
+   }
+}
