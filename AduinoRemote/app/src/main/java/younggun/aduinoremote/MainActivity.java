942cd6b861d1981d9387efc40f9db90c4059dd91
younggunwon <dnjsdudrjs@naver.com>
2017-05-24 00:55:13 +0900

    add
    find

--- AduinoRemote/app/src/main/java/younggun/aduinoremote/MainActivity.java
+++ AduinoRemote/app/src/main/java/younggun/aduinoremote/MainActivity.java
@@ -2,13 +2,27 @@ package younggun.aduinoremote;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothDevice mBluetoothDevice;
+    MainFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +31,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        setBluetoothAdapter();
+
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.layout_main, new MainFragment());
         ft.commit();
+
+
     }
 
     @Override
@@ -44,4 +62,79 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    void setDevice() {
+        mainFragment = (MainFragment)getFragmentManager().findFragmentById(R.id.layout_main);
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            mainFragment.setPairedDevice(pairedDevices);
+        }
+        findDevice();
+    }
+
+    void findDevice() {
+        if(mBluetoothAdapter.isDiscovering()) {
+            mBluetoothAdapter.cancelDiscovery();
+            unregisterReceiver(mReceiver);
+        }
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothDevice.ACTION_NAME_CHANGED);
+        filter.addAction(BluetoothDevice.ACTION_CLASS_CHANGED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, filter);
+        mBluetoothAdapter.startDiscovery();
+    }
+
+    void setBluetoothAdapter() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(MainActivity.this, "블루투스를 지원하지 않는 기기입니다.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                Log.e("find","1");
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mainFragment.addDevice(device);
+            } else if (BluetoothDevice.ACTION_NAME_CHANGED.equals(action)) {
+                Log.e("find","2");
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mainFragment.addDevice(device);
+            } else if (BluetoothDevice.ACTION_CLASS_CHANGED.equals(action)) {
+                Log.e("find","3");
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mainFragment.addDevice(device);
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.e("find","finish");
+                if(mainFragment.mArrayAdapter.isEmpty()) {
+                    mainFragment.mArrayAdapter.add("연결 가능한 장치가 없습니다.");
+                }
+                unregisterReceiver(mReceiver);
+            }
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 100);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 100 && resultCode == RESULT_CANCELED) {
+            Toast.makeText(MainActivity.this, "블루투스를 사용해야 합니다.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
 }
+
+//연결 가능한 장치가 있을경우
--- AduinoRemote/app/src/main/java/younggun/aduinoremote/MainFragment.java
+++ AduinoRemote/app/src/main/java/younggun/aduinoremote/MainFragment.java
@@ -1,12 +1,17 @@
 package younggun.aduinoremote;
 
 import android.app.Fragment;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Set;
 
 /**
  * Created by LOVE on 2017-05-23.
@@ -14,17 +19,41 @@ import android.view.ViewGroup;
 
 public class MainFragment extends Fragment {
 
+    View v;
+    ArrayAdapter mArrayAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        v = inflater.inflate(R.layout.fragment_main, container, false);
         FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        ListView lv_find = (ListView)v.findViewById(R.id.lv_find);
+        mArrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+        lv_find.setAdapter(mArrayAdapter);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //주변 기기 검색
+                mArrayAdapter.clear();
+                ((MainActivity)getActivity()).setDevice();
             }
         });
         return v;
+   }
+
+   void setPairedDevice(Set<BluetoothDevice> pairedDevices) {
+
+       for (BluetoothDevice device : pairedDevices) {
+           addDevice(device);
+       }
+   }
+
+   void addDevice(BluetoothDevice device) {
+       mArrayAdapter.add(device.getName() + "    " + device.getAddress());
+   }
+
+   void checkEmpty() {
+       if(mArrayAdapter.isEmpty()) {
+           mArrayAdapter.add("연결 가능한 기기가 없습니다.");
+       }
    }
 }
